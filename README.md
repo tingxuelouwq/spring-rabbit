55c21dbc31ebe496c163337d4fb45c4402aacf97
Kevin Smih <kevin_wangqi@126.com>
2018-01-09 10:53:19 +0800

    spring集成rabbitmqq

---
+++ README.md
@@ -0,0 +1,1 @@
+## Spring集成RabbitMQ
---
+++ sync-xml/src/main/java/com/kevin/spring/rabbit/producer/Producer.java
@@ -0,0 +1,25 @@
+package com.kevin.spring.rabbit.producer;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @class: Producer
+ * @package: com.kevin.spring.producer
+ * @author: kevin[wangqi2017@xinhua.org]
+ * @date: 2017/12/4 20:25
+ * @version: 1.0
+ * @desc:
+ */
+public class Producer {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-context.xml");
+        AmqpTemplate amqpTemplate = context.getBean(RabbitTemplate.class);
+        String message = "test spring sync message";
+        amqpTemplate.convertAndSend(message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
